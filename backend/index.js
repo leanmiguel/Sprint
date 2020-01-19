4ab7bdd68329863c569bad1874dc76861152cb37
Lean Miguel <theleanmiguel@gmail.com>
2020-01-18 22:12:35 -0700

    added express server

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
+app.listen(port, () => console.log(`App is running on ${port}!`));

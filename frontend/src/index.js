a9e3cb6ddc6dd0b3dd7bcff119f7340b0bfe981a
leanmiguel <theleanmiguel@gmail.com>
2020-04-15 19:31:02 -0600

    remove auth0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,52 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
 
-import ApolloClient from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
+import { ApolloProvider } from "@apollo/react-hooks"
 
-import { Auth0Provider } from "./react-auth0-spa";
-import config from "./auth_config.json";
-import history from "./utils/history";
+import history from "./utils/history"
 
 const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.REACT_APP_DB_ENDPOINT,
     headers: {
-      "x-hasura-admin-secret": process.env.REACT_APP_ADMIN_SECRET
-    }
+      "x-hasura-admin-secret": process.env.REACT_APP_ADMIN_SECRET,
+    },
   }),
-  cache: new InMemoryCache()
-});
+  cache: new InMemoryCache(),
+})
 // A function that routes the user to the right place
 // after login
-const onRedirectCallback = appState => {
+const onRedirectCallback = (appState) => {
   history.push(
     appState && appState.targetUrl
       ? appState.targetUrl
       : window.location.pathname
-  );
-};
+  )
+}
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
-    redirect_uri={window.location.origin}
-    onRedirectCallback={onRedirectCallback}
-  >
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </Auth0Provider>,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById("root")
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()

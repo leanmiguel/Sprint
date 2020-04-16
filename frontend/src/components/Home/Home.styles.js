95bd8ede121460e6afd58a2d4b666d270819396f
ajsantos08 <arthurjason08@gmail.com>
2020-04-15 21:45:28 -0600

    scrollable cards

--- frontend/src/components/Home/Home.styles.js
+++ frontend/src/components/Home/Home.styles.js
@@ -1,9 +1,20 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export const ProjectsContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
+	height: 100%;
+	white-space: nowrap;
+	box-sizing: border-box;
+	min-width: 100%;
+	overflow-x: scroll;
 `
-export const Header = styled.h1`
-  text-align: center;
+export const Header = styled.h1`text-align: center;`
+
+export const AddContainer = styled.div`
+	display: inline-block;
+	flex-direction: column;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	transition: 0.3s;
+	padding: 20px;
+	width: 500px;
+	flex-shrink: 0;
 `

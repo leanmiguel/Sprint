223a9f7897d79a5e8e226abdf8c0ff093242f5b2
Lean Miguel <theleanmiguel@gmail.com>
2020-04-21 09:01:20 -0600

    feat(gql server): be able to add user through gql

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 // setup
-require("dotenv").config()
+require("dotenv").config();
 
 //graphql setup
 
-const { ApolloServer, gql } = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server");
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -16,36 +16,49 @@ const typeDefs = gql`
   type Query {
     users: [User]
   }
-`
+  type Mutation {
+    addUser(name: String!): User!
+  }
+`;
 
 // postgres setup
-const { Client } = require("pg")
+const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-})
+});
 
-client.connect()
+client.connect();
 
 const getUsers = async () => {
-  const info = await client.query("SELECT * FROM users")
-  return info.rows
-}
+  const info = await client.query("SELECT * FROM users");
+  return info.rows;
+};
 
 const resolvers = {
   Query: {
     users: getUsers,
   },
-}
+  Mutation: {
+    addUser: async (parent, args) => {
+      const pgQuery = {
+        text: "INSERT INTO users(name) VALUES($1) RETURNING *",
+        values: [args.name],
+      };
+      const info = await client.query(pgQuery);
+      return info.rows[0];
+    },
+  },
+};
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+  console.log(`🚀  Server ready at ${url}`);
+});

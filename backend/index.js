97545dce0aaeb9496f14b528f34d9b92f7ed3339
Lean Miguel <theleanmiguel@gmail.com>
2020-04-21 19:51:53 -0600

    feat(gql): update mutators to have full crud

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 // setup
-require("dotenv").config();
+require("dotenv").config()
 
 //graphql setup
 
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server")
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -18,47 +18,63 @@ const typeDefs = gql`
   }
   type Mutation {
     addUser(name: String!): User!
+    updateUser(name: String!, id: ID!): User!
+    deleteUser(id: ID!): User!
   }
-`;
+`
 
 // postgres setup
-const { Client } = require("pg");
+const { Client } = require("pg")
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-});
+})
 
-client.connect();
+client.connect()
 
 const getUsers = async () => {
-  const info = await client.query("SELECT * FROM users");
-  return info.rows;
-};
+  const info = await client.query("SELECT * FROM users")
+  return info.rows
+}
 
 const resolvers = {
   Query: {
     users: getUsers,
   },
   Mutation: {
-    addUser: async (parent, args) => {
+    addUser: async (_, args) => {
       const pgQuery = {
         text: "INSERT INTO users(name) VALUES($1) RETURNING *",
         values: [args.name],
-      };
-      const info = await client.query(pgQuery);
-      return info.rows[0];
+      }
+      const info = await client.query(pgQuery)
+      return info.rows[0]
+    },
+    updateUser: async (_, args) => {
+      const pgQuery = {
+        text: `UPDATE users SET "name" = '${args.name}' WHERE "id" = ${args.id} RETURNING *`,
+      }
+      const info = await client.query(pgQuery)
+      return info.rows[0]
+    },
+    deleteUser: async (_, args) => {
+      const pgQuery = {
+        text: `DELETE FROM users WHERE ("id" = ${args.id}) RETURNING *`,
+      }
+      const info = await client.query(pgQuery)
+      return info.rows[0]
     },
   },
-};
+}
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})

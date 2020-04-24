82a729fcd56b74d3c0464bf18a8f2b526a6ecf6a
leanmiguel <theleanmiguel@gmail.com>
2020-04-23 18:26:06 -0600

    feat(index): refactor the index, using modules (cjs)

--- backend/index.js
+++ backend/index.js
@@ -3,78 +3,12 @@ require("dotenv").config()
 
 //graphql setup
 
-const { ApolloServer, gql } = require("apollo-server")
+const { ApolloServer } = require("apollo-server")
+const { resolvers } = require("./graphql/resolvers")
+const { typeDefs } = require("./graphql/typeDefs")
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  type User {
-    id: ID
-    name: String
-  }
-  type Query {
-    users: [User]
-  }
-  type Mutation {
-    addUser(name: String!): User!
-    updateUser(name: String!, id: ID!): User!
-    deleteUser(id: ID!): User!
-  }
-`
-
-// postgres setup
-const { Client } = require("pg")
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
-
-client.connect()
-
-const getUsers = async () => {
-  const info = await client.query("SELECT * FROM users")
-  return info.rows
-}
-
-const resolvers = {
-  Query: {
-    users: getUsers,
-  },
-  Mutation: {
-    addUser: async (_, args) => {
-      const pgQuery = {
-        text: "INSERT INTO users(name) VALUES($1) RETURNING *",
-        values: [args.name],
-      }
-      const info = await client.query(pgQuery)
-      return info.rows[0]
-    },
-    updateUser: async (_, args) => {
-      const pgQuery = {
-        text: `UPDATE users SET "name" = '${args.name}' WHERE "id" = ${args.id} RETURNING *`,
-      }
-      const info = await client.query(pgQuery)
-      return info.rows[0]
-    },
-    deleteUser: async (_, args) => {
-      const pgQuery = {
-        text: `DELETE FROM users WHERE ("id" = ${args.id}) RETURNING *`,
-      }
-      const info = await client.query(pgQuery)
-      return info.rows[0]
-    },
-  },
-}
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers })
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })

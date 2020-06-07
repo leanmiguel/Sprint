c92a1144b21b9eef145be34dc1a7fc7a71e50a21
Lean <theleanmiguel@gmail.com>
2020-06-07 17:14:20 -0600

    feat(sprint): set up next js frontend

---
+++ next-frontend/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Finish Your Projects Faster</h1>
+    </div>
+  );
+};
+
+export default Home;

eb40903778a81df7c9af1854ca0855a3a4163eea
Lean <theleanmiguel@gmail.com>
2020-06-20 13:45:21 -0600

    feat(global styles): add font to global styles

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,36 +1,32 @@
-import Head from 'next/head'
-import mock from '../images/mock.jpg'
+import Head from "next/head"
+import mock from "../images/mock.jpg"
 
 const Home = () => {
-	return (
-		<div>
-			<Head>
-				<title>sprint</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<div>
-				<h1>Finish Your Projects Faster</h1>
-				<h4>
-					Sprint is the best way to supercharge your productivity. Collaborate with others, knock out your
-					goals, and complete projects faster with Sprint.{' '}
-				</h4>
-				<button>Try It Now</button>
-			</div>
-			<img src={mock} alt="mock" id="mock" />
-			<style jsx>{`
-				@font-face {
-					font-family: 'rabbidhighwaysignii';
-					src: url('/fonts/rabbidhighwaysignii-webfont.woff');
-					src: url('/fonts/rabbidhighwaysignii-webfont.woff2');
-				}
-				h1,
-				h4,
-				button {
-					font-family: 'rabbidhighwaysignii';
-				}
-			`}</style>
-		</div>
-	)
+  return (
+    <div>
+      <Head>
+        <title>sprint</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Finish Your Projects Faster</h1>
+        <h4>
+          Sprint is the best way to supercharge your productivity. Collaborate
+          with others, knock out your goals, and complete projects faster with
+          Sprint.
+        </h4>
+        <button>Try It Now</button>
+      </div>
+      <img src={mock} alt="mock" id="mock" />
+      <style jsx>{`
+        h1,
+        h4,
+        button {
+          font-family: "rabbidhighwaysignii";
+        }
+      `}</style>
+    </div>
+  )
 }
 
 export default Home
